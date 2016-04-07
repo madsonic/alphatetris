5864899bdfebf64bb0d64c167d0dbf3af2920bcc
Teo Qi Xuan <teoqixuan@hotmail.com>
2016-04-08 01:46:20 +0800

    added crossover and mutate methods, updated partial player 2 for testing

--- src/main/GeneticTrainer.java
+++ src/main/GeneticTrainer.java
@@ -1,5 +1,6 @@
 package main;
 import java.util.Arrays;
+import java.util.Random;
 
 
 
@@ -7,26 +8,43 @@ public class GeneticTrainer {
     
     private PartialPlayerTwo player = new PartialPlayerTwo();
     
-    private final static int MAX_WEIGHT_VALUE = 1000;
-    private final static int MAX_NUM_WEIGHTS = 100;
-    private final static int NUM_HEURISTICS = 4;
-    private final static int PERCENT_BEST_REPRODUCE = 10;
-    private double[][] weights = new double[MAX_NUM_WEIGHTS][NUM_HEURISTICS];
-    private Score[] scores = new Score[MAX_NUM_WEIGHTS];
+    private final static double MAX_WEIGHT_VALUE = 100;
+    private final static int NUM_HEURISTICS = 7;
+    private final static int PERCENT_BEST_SELECT = 10;
     
-    //initialize basic weights spread across the max weight values
+    private final static int INIT_NUM_OF_TESTS = 100;
+    private final static int MAX_NUM_NEW_TESTS = INIT_NUM_OF_TESTS / 10 * 3;
+    
+    private final static int PERCENT_MUTATION_CHANCE = 10;
+    private final static double MAX_MUTATION = 0.2;
+    
+    private double[][] tests = new double[INIT_NUM_OF_TESTS][NUM_HEURISTICS];
+    private Score[] scores = new Score[INIT_NUM_OF_TESTS + MAX_NUM_NEW_TESTS];
+    private Score[] newScores = new Score[MAX_NUM_NEW_TESTS];
+    
+    private int numTests;
+    private int numNewTests;
+    private Random random;
+    
+    public GeneticTrainer() {
+        numTests = INIT_NUM_OF_TESTS;
+        numNewTests = 0;
+        random = new Random();
+    }
+    //initialize basic weights spread across all possible weight values
     public void initializeWeights() {
-        for (int i = 0; i < MAX_NUM_WEIGHTS; i += (MAX_WEIGHT_VALUE / MAX_NUM_WEIGHTS)) {
-            weights[i] = new double[] { i, i, i, i};
+        for (int i = 0; i < INIT_NUM_OF_TESTS; i += (2 * MAX_WEIGHT_VALUE / INIT_NUM_OF_TESTS)) {
+            //adjust for negative values
+            double weight = i - MAX_WEIGHT_VALUE;
+            tests[i] = new double[] { weight, weight, weight, weight, weight, weight, weight};
         }
     }
     
     //get the scores for the weights
     public void runGame() {
-        for (int i = 0; i < MAX_NUM_WEIGHTS; i++) {
-            scores[i] = new Score(player.playGame(weights[i]), i);
+        for (int i = 0; i < numTests; i++) {
+            scores[i] = new Score(player.playGame(tests[i]), i);
         }
-        Arrays.sort(scores);
     }
     
     private class Score implements Comparable<Score>{
@@ -49,9 +67,9 @@ public class GeneticTrainer {
             }
         }
         
-        public int getIndex() {
+        /*public int getIndex() {
             return index;
-        }
+        }*/
         
         public String toString() {
             return score + ", " + index;
@@ -59,18 +77,52 @@ public class GeneticTrainer {
         
     }
     
-    //select best % of the weights for reproduction
-    public void produceOffspring() {
-        for (int i = 1; i <= MAX_NUM_WEIGHTS/PERCENT_BEST_REPRODUCE; i++) {
-            System.out.println(scores[MAX_NUM_WEIGHTS - i]);
+    //select best % of the weights for reproduction - not v sophisticated
+    public void truncationSelection() {
+        Arrays.sort(scores);
+        for (int i = 1; i <= numTests/PERCENT_BEST_SELECT; i++) {
+            System.out.println(scores[INIT_NUM_OF_TESTS - i]);
         }
     }
     
+    // crossovers two sets of tests at one single point, then mutate each new one
+    public void crossoverAndMutate(double[] test1, double[] test2) {
+        double[] newTest1 = new double[NUM_HEURISTICS];
+        double[] newTest2 = new double[NUM_HEURISTICS];
+        
+        //finds point at which to crossover between index [1 to NUM_HEURISTICS - 1]
+        int crossoverIndex = random.nextInt(NUM_HEURISTICS - 1) + 1; 
+        
+        for (int i = 0; i < NUM_HEURISTICS; i++) {
+            if (i < crossoverIndex) {
+                newTest1[i] = mutate(test1[i]);
+                newTest2[i] = mutate(test2[i]);
+            } else {
+                newTest1[i] = mutate(test2[i]);
+                newTest2[i] = mutate(test1[i]);
+            }
+        }
+        
+    }
+    
+    public double mutate(double value) {
+        if (random.nextInt(100) < PERCENT_MUTATION_CHANCE) {
+            //randomly add a value within +-MAX_MUTATION
+            value += (random.nextDouble() * MAX_MUTATION * 2) - MAX_MUTATION;
+        }
+        return value;
+    }
+    
+    
+    
     public static void main(String[] args) {
-        GeneticTrainer meghanTrainor = new GeneticTrainer();
-        meghanTrainor.initializeWeights();
-        meghanTrainor.runGame();
-        meghanTrainor.produceOffspring();
+        GeneticTrainer geneticTrainer = new GeneticTrainer();
+        //geneticTrainer.initializeWeights();
+        //geneticTrainer.runGame();
+        //geneticTrainer.truncationSelection();
+        double[] test1 = new double[] {1, 1, 1, 1, 1, 1, 1};
+        double[] test2 = new double[] {2, 2, 2, 2, 2, 2, 2};
+        
         
         
     }
--- src/main/PartialPlayerTwo.java
+++ src/main/PartialPlayerTwo.java
@@ -1,48 +1,59 @@
 package main;
-//same as Player Two but with early termination after x moves.
+// exactly the same as Player Two but with early termination after x moves.
+
 public class PartialPlayerTwo {
     public static final int MAX_MOVES = 10;
     
     public int playGame(double[] weights) {
         State s = new State();
 
-        // initialize root node's parent bn
-        BitBoardCol.initPieceBits();
-        BitBoardCol bb = new BitBoardCol(new int[10], s.getTop());
-        BoardNode bn = new BoardNode(null, bb, new int[] { 1, 1 });
-        bb.setWeights(weights);
+        int total_reward = 0;
         
-        // initialize root
-        // root always points to a PieceNode
+        //Set static weights and childNum
+        BitBoardCol.setWeights(weights);
         PieceNode.setChildNum(2);
+
+        
+        //initialize root node's parent bn
+        //BitBoardCol.initPieceBits();
+        BitBoardCol bb = new BitBoardCol(new int[10], new int[10] );
+        BoardNode bn = new BoardNode(bb);
+        
+        //initialize root
+        //root always points to a PieceNode
         PieceNode root = new PieceNode(bn, s.nextPiece);
 
-        // Initialize depth
-        // every call to rootExpand expands the tree by 1 layer
-        // uncomment to increase depth
+        //Initialize depth
+        //every call to rootExpand expands the tree by 1 layer
+        //uncomment to increase depth
+        //last call must be rootExpandAndUpdate
 
         root.rootExpand();
-        // root.rootExpand();
-        // root.rootExpand();
-        // root.rootExpand();
+        //root.rootExpand();
+        //root.rootExpand();
+        //root.rootExpand();
+        root.rootExpandAndUpdate();
 
-        while (!s.hasLost() && s.getTurnNumber() < MAX_MOVES) {
-            root.rootExpand();
-
-            // update ExpectiMiniMax values
-            root.rootUpdate();
-
+        while(!s.hasLost() && s.getTurnNumber() < MAX_MOVES) {
+            /*
+            if (s.getTurnNumber()%1000 == 500) {
+                System.out.println("Turn num: "+ s.getTurnNumber()
+                        + "   Avg reward: " + total_reward/s.getTurnNumber()
+                        + "   Lines cleared: " + s.getRowsCleared());
+            }
+            */
+            //update ExpectiMiniMax values
+            //root.rootUpdate();
             s.makeMove(root.getBestMove());
-            // print_field(s);
-            root = root.setRootToBest(s.nextPiece);
 
-            // try {
-            // Thread.sleep(300);
-            // } catch (InterruptedException e) {
-            // e.printStackTrace();
-            // }
+            total_reward+= root.bestNode.bb.getReward();
+            root = root.setRootToBest(s.nextPiece);
+            //root.rootExpand();
+            root.rootExpandAndUpdate();
         }
+        System.out.println("You have completed "+s.getRowsCleared()+" rows.");
         return s.getRowsCleared();
+        //return total_reward;
     }
 
 }
