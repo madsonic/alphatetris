57e635510324a6236dfe94ed91a1818bdd50604d
Vincent <vin9492@hotmail.com>
2016-04-08 23:25:41 +0800

    fixed build breaks

--- src/pso/GameSimulation.java
+++ src/pso/GameSimulation.java
@@ -14,37 +14,44 @@ public class GameSimulation {
 	
 	public int start(double[] weights) {
         State s = new State();
-
-        // initialize root node's parent bn
-        BitBoardCol bb = new BitBoardCol(new int[10], s.getTop());
-        BoardNode bn = new BoardNode(null, bb, new int[] { 1, 1 });
-        bb.setWeights(weights);
         
-        // initialize root
-        // root always points to a PieceNode
-        PieceNode.setChildNum(2);
-        PieceNode root = new PieceNode(bn, s.nextPiece);
+        int total_reward = 0;
 
-        // Initialize depth
-        // every call to rootExpand expands the tree by 1 layer
-        // uncomment to increase depth
+		//Set static weights and childNum
+		BitBoardCol.setWeights(weights);
+		PieceNode.setChildNum(2);
+        
+		//initialize root node's parent bn
+		//BitBoardCol.initPieceBits();
+		BitBoardCol bb = new BitBoardCol(new int[10], new int[10] );
+		BoardNode bn = new BoardNode(bb);
+		
+		//initialize root
+		//root always points to a PieceNode
+		PieceNode root = new PieceNode(bn, s.nextPiece);
 
-        root.rootExpand();
-        // root.rootExpand();
-        // root.rootExpand();
-        // root.rootExpand();
+		//Initialize depth
+		//every call to rootExpand expands the tree by 1 layer
+		//uncomment root.rootExpand() to increase look-ahead by 1
+		//last call must be rootExpandAndUpdate
+
+		//root.rootExpand();
+		//root.rootExpand();
+		root.rootExpandAndUpdate();
 
         while (!s.hasLost() && s.getTurnNumber() < MAX_MOVES) {
-            root.rootExpand();
 
             // update ExpectiMiniMax values
-            root.rootUpdate();
-
+            //root.rootUpdate();
             s.makeMove(root.getBestMove());
-            // print_field(s);
+
+            total_reward+= root.bestNode.bb.getReward();
             root = root.setRootToBest(s.nextPiece);
+			//root.rootExpand();
+			root.rootExpandAndUpdate();
         }
         System.out.println("You have completed "+s.getRowsCleared()+" rows.");
         return s.getRowsCleared();
+        //return total_reward;
 	}
 }
--- src/pso/MyParticle.java
+++ src/pso/MyParticle.java
@@ -4,6 +4,6 @@ import net.sourceforge.jswarm_pso.Particle;
 
 public class MyParticle extends Particle {
 	public MyParticle() {
-		super(4);
+		super(7);
 	}
 }
