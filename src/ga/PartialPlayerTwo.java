2c9107c53993db35670bd9458e455a8552d86671
Gerald <ngjingsiang@gmail.com>
2016-04-11 10:12:35 +0800

    added method to run play as partial player 2

--- src/ga/PartialPlayerTwo.java
+++ src/ga/PartialPlayerTwo.java
@@ -4,6 +4,7 @@ import main.BitBoardCol;
 import main.BoardNode;
 import main.PieceNode;
 import main.State;
+import main.TFrame;
 
 // exactly the same as Player Two but with option of early termination after x moves.
 
@@ -63,6 +64,7 @@ public class PartialPlayerTwo {
     // plays the game for real and returns rows cleared
     public int playGame(double[] weights) {
         State s = new State();
+        new TFrame(s);
 
         // Set static weights and childNum
         BitBoardCol.setWeights(weights);
@@ -91,16 +93,39 @@ public class PartialPlayerTwo {
         while (!s.hasLost()) {
             
              if (s.getTurnNumber()%2000 == 500) {
-              System.out.println("Turn num: "+ s.getTurnNumber() +
-              "   Lines cleared: " + s.getRowsCleared()); }
+	              System.out.println("Turn num: "+ s.getTurnNumber() +"   Lines cleared: " + s.getRowsCleared()); 
+              }
             // update ExpectiMiniMax values
             // root.rootUpdate();
             s.makeMove(root.getBestMove());
+            
+            // turn by turn graphics
+//            s.draw();
+//            s.drawNext(0,0);
+            
             root = root.setRootToBest(s.nextPiece);
             // root.rootExpand();
             root.rootExpandAndUpdate();
         }
+        System.out.println("I'm dead");
+        s.draw();
         return s.getRowsCleared();
 
+    }
+    
+    public static void main(String[] args) {
+    	// play game
+    	PartialPlayerTwo p = new PartialPlayerTwo();
+    	double[] weights = {1.550894525201647,
+    	                    3.6393568332418647,
+    	                    9.793561450312492,
+    	                    8.146038155910917,
+    	                    5.711970387950983,
+    	                    2.1999248114171865,
+    	                    0.9223982639211392,
+    	                    4.220457292054116,
+    	                    0.3770283420907572,
+    	                    1.5238002012538043};
+    	p.playGame(weights);
     }
 }
